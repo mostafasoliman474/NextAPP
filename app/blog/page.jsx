0eb6ff5eceaacc22778bdf01e26274a572a94cd7
mostafasoliman474 <as6615150@gmail.com>
2023-10-20 20:41:19 +0300

    blog img

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -35,13 +35,14 @@ const Blog = async() => {
   return (
     <div className='flex flex-col gap-16 my-8 h-auto' >
       {data.map((item)=>(
-        <Link href={`blog/${item._id}`} key={item._id} className='flex gap-11 h-[40vh] '>
-        <div className='w-[40%] relative'>
+        <Link href={`blog/${item._id}`} key={item._id} className='flex gap-11 sm:h-[40vh] sm:flex-row flex-col'>
+        <div className='sm:w-[40%] sm:relative w-[100%] h-[300px]'>
           <Image
-            fill={true}
+            width={400}
+            height={200}
             src={item.img}
             alt='hi'
-            className='object-cover rounded '
+            className='object-cover rounded relative h-[100%] w-[100%]'
           />
         </div>
         <div  className='w-[60%] flex flex-col justify-center'>
