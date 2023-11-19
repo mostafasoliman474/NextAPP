bf1174fb58e3e2031b5eac0fb00cb825b7831049
mostafasoliman474 <as6615150@gmail.com>
2023-11-19 20:27:43 +0200

    Green btn and padding

--- app/portfolio/[category]/page.jsx
+++ app/portfolio/[category]/page.jsx
@@ -21,7 +21,7 @@ const Category = ({ params }) => {
       {data.map((item) => (
         
         <div className='flex gap-[32px] sm:h-[40vh] sm:flex-row-reverse flex-col-reverse' key={item.id} >
-          <div className='flex-1 p-[20px_0_0_20px] flex justify-center flex-col tracking-widest'>
+          <div className='flex-1 sm:p-[20px_0_0_20px] p-[20px_0_0_0] flex justify-center flex-col tracking-widest'>
             <h1 className='font-extrabold text-5xl '>{item.title}</h1>
             <p className='max-w-[90%] leading-6 my-5'>{item.desc}</p>
             <Button url='/' text='see more' />
